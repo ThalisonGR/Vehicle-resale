d34a73aaec24729cbbc3df20951b2cc69c053140
Thalison <Tec.thalison@gmail.com>
2024-03-24 14:51:36 -0300

    feat: Implementing service Category Vehicle, saving

--- revenda/src/main/java/br/com/revenda/service/entities/CategoryVehicleService.java
+++ revenda/src/main/java/br/com/revenda/service/entities/CategoryVehicleService.java
@@ -1,27 +1,45 @@
 package br.com.revenda.service.entities;
 
+import br.com.revenda.domain.entities.CategoryVehicle;
+import br.com.revenda.dto.CategoryVehicleDTO;
+import br.com.revenda.respository.CategoryVehicleRepository;
 import br.com.revenda.service.interfaces.ICRUD;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
-public class CategoryVehicleService implements ICRUD {
+@Service
+public class CategoryVehicleService implements ICRUD<CategoryVehicle, Integer , CategoryVehicleDTO> {
+    private Logger logger;
+    @Autowired
+    private CategoryVehicleRepository categoryVehicleRepository;
     @Override
-    public Object save(Object object) {
+    public CategoryVehicle save(CategoryVehicleDTO categoryVehicleDTO) {
+        try {
+            CategoryVehicle categoryVehicle = new CategoryVehicle(categoryVehicleDTO);
+            categoryVehicleRepository.save(categoryVehicle);
+            return categoryVehicle;
+            s
+        }catch (Exception e){
+            logger.info("CategoryVehicleService: " + e.getMessage());
+        }
+        return  null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public CategoryVehicle update(String id, CategoryVehicleDTO Object) {
         return null;
     }
 
     @Override
-    public Object update(String id, Object object) {
-        return null;
-    }
-
-    @Override
-    public void delete(Object id) {
-
-    }
-
-    @Override
-    public Optional getById(Object id) {
+    public Optional<CategoryVehicle> getById(Integer id) {
         return Optional.empty();
     }
 }
